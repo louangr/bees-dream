9d3e4a05dc1b45a7efafb9d74e44a214151784c5
lngr <lemail@de.lngr>
2022-05-24 16:47:15 +0200

    add shared style

---
+++ monodose-webapp/src/shared/styles.ts
@@ -0,0 +1,9 @@
+export const styles: { [key: string]: React.CSSProperties } = {
+    container: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+}
