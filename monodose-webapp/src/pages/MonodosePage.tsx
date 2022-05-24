c82a606b2f15aa0c43df0914ce990e44ecc1ef39
dcaruso8 <daryltron@gmail.com>
2022-05-24 17:42:31 +0200

    maquette monodose info

--- monodose-webapp/src/pages/MonodosePage.tsx
+++ monodose-webapp/src/pages/MonodosePage.tsx
@@ -1,11 +1,22 @@
 import '../assets/info/styles/MonodosePage.css'
+//import '../assets/info/styles/ScreenFormat.css'
 import Info from '../components/Info'
 import Apiculteur from '../assets/info/images/apiculteur.png'
+import Localisation from '../assets/info/images/Localisation.png'
+import DateFabrication from '../assets/info/images/Date_Fabrication.png'
+import Date_DLUO from '../assets/info/images/Date_DLUO.png'
+import Variete from '../assets/info/images/Variete.png'
 
 const MonodosePage: React.FC = () => {
     return (
         <div className='monodose-page-container'>
-            <Info icon={Apiculteur} description='Apiculteur' informations={['Dorian','Beerian','23']}/>
+            <div className='alveole'>
+                <Info icon={Apiculteur} description='Apiculteur' informations={['Dorian','Beerian','23']}/>
+                <Info icon={Localisation} description='Localisation' informations={['Nantes']}/>
+                <Info icon={DateFabrication} description='Date de fabrication' informations={['23/05/2020']}/>
+                <Info icon={Date_DLUO} description='Date DLUO' informations={['23/05/2022']}/>
+                <Info icon={Variete} description='Variété' informations={['Acacia']}/>
+            </div>
         </div>
     )
 }
