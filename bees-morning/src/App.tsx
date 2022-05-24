cba5a0f03043b8914eb12703bd312565423b1002
Gaspar Mis <gaspar.missiaen@hotmail.fr>
2022-05-24 17:49:50 +0200

    :wrench: Regenerate react project and init project

---
+++ bees-morning/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Presentation from "./pages/Presentation";
+import "./App.css";
+
+function App() {
+  return <Presentation />;
+}
+
+export default App;
---
+++ bees-morning/src/pages/Presentation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Presentation = () => {
+  return (
+    <div className="content">
+      <p>Todo</p>
+    </div>
+  );
+};
+
+export default Presentation;
