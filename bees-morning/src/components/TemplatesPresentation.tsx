6d429c0cc375f1f8a1038f95277076e35e2266d4
Gaspar Mis <gaspar.missiaen@hotmail.fr>
2022-05-25 10:51:36 +0200

    :sparkles: Retrieve data from prismic from presentation

--- bees-morning/src/components/TemplatesPresentation.tsx
+++
@@ -1,5 +0,0 @@
-const TemplatesPresentation = () => {
-  return <div className="test"></div>;
-};
-
-export default TemplatesPresentation;
--- bees-morning/src/pages/Presentation.tsx
+++ bees-morning/src/pages/Presentation.tsx
@@ -1,10 +1,58 @@
-import { usePrismicDocumentByUID } from "@prismicio/react";
-import TemplatesPresentation from "../components/TemplatesPresentation";
+import { PrismicRichText, usePrismicDocumentByUID } from "@prismicio/react";
+import { SliceZone } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
 
 const Presentation = () => {
   const [document] = usePrismicDocumentByUID("display-unit", "presentation");
 
-  return <TemplatesPresentation />;
+  function TextSliceType1({ slice }: any) {
+    return (
+      <section>
+        <div className="left-info">
+          <PrismicRichText field={slice.primary.left_text} />
+        </div>
+        <div className="right-info">
+          <PrismicRichText field={slice.primary.right_text} />
+        </div>
+      </section>
+    );
+  }
+
+  function TextSliceType2({ slice }: any) {
+    const src = prismicH.asImageSrc(slice.primary.image);
+    if (src !== null) {
+      return (
+        <section>
+          <img src={src} alt={slice.primary.image.alt} />
+        </section>
+      );
+    } else {
+      return (
+        <section>
+          <img alt={slice.primary.image.alt} />
+        </section>
+      );
+    }
+  }
+
+  function TextSliceType3({ slice }: any) {
+    return (
+      <section>
+        <PrismicRichText field={slice.primary.center_text} />
+      </section>
+    );
+  }
+
+  return (
+    <SliceZone
+      slices={document?.data.body}
+      components={{
+        presentation_type_1: TextSliceType1,
+        presentation_type_2: TextSliceType2,
+        presentation_type_3: TextSliceType3,
+      }}
+    />
+  );
 };
 
 export default Presentation;
