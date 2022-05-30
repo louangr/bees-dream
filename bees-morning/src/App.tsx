0abdfd6560804101b5ecdf7ccad9773285cbf21b
Antoine <antoine.rochefort@etu.univ-nantes.fr>
2022-05-30 14:46:30 +0200

    :construction: UseEffect question for rotation

--- bees-morning/src/App.tsx
+++ bees-morning/src/App.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Presentation from "./pages/Presentation";
 import "./styles/_app.scss";
 import Questions from "./components/Questions";
+import { usePrismicDocumentByUID } from "@prismicio/react";
+
+let length = 0;
 
 function App() {
+  const [document] = usePrismicDocumentByUID("quizz-question", "questions");
+  const [index, setIndex] = useState(0);
+
+  length = document?.data.body?.length;
+  let q = document?.data.body?.["0"].primary.question?.["0"].text;
+
+useEffect(()=>{
+  const interval = setInterval(() => {
+    console.log("length :", length);
+    console.log(index <= length-1);
+    if(index <= length-1){
+      setIndex (index => index + 1);
+      console.log("index if:", index);
+    } 
+     else if(index >= length){
+      setIndex (0)
+    }
+  }, 2000);
+  return () => clearInterval(interval);
+}, [])
+
+console.log("Index :", index)
   return (
     <div>
       <h1 className="title">Bee's Morning</h1>
       <Presentation />
-      <Questions/>
+      <Questions question={document?.data.body?.[index].primary}/>
     </div>
   );
 }
--- bees-morning/src/components/Questions.tsx
+++ bees-morning/src/components/Questions.tsx
@@ -1,18 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../styles/Questions.css";
 import Alveole from "../assets/image/abeille-svg-yellow.svg"
 import Logo from "../assets/image/LogoBeesDream.webp";
+import { PrismicRichText, usePrismicDocumentByUID } from "@prismicio/react";
 
-const Questions = () => {
+interface IQuestion{
+    question: string
+}
+
+const Questions = ({question}: IQuestion) => {
+
+    const [doc] = usePrismicDocumentByUID("quizz-question", "questions");
+    console.log(question)
+    /* function refreshImage(imgElement, imgURL){     
+        var timestamp = new Date().getTime();  
+      
+        var el = document.getElementById(imgElement);  
+      
+        var queryString = "?t=" + timestamp;    
+       
+        el.src = imgURL + queryString;    
+    } */
+
+    const alv = document.querySelector(".s1");
+    const questFromPris = ()=>{
+        for(let i=0; doc?.data.body?.length; i++){
+            //console.log(i);
+            question = doc?.data.body?.[i].primary.question?.["0"].text;
+           /*  reponse = doc?.data.body?.[i].primary.answer?.["0"].text;
+            console.log(doc)
+            if (flag){
+                return <p>{reponse}</p>
+                console.log("Réponse", i+1, ":", reponse)
+            } */
+            //console.log("Question", i+1, ":", question)
+            return <p>{question}</p>
+        }
+    }
+    
+    const [flag, setflag] = useState(false)
+
+   /*  const displayQuestion = ()=>{
+        if (flag){
+            return <p>{reponse}</p>
+
+        }
+        return <p>{question}</p>
+    } */
+
+    const change = ()=>{
+        setTimeout(() => {
+            setflag (true)
+        }, 9000);
+        setTimeout(() => {
+            setflag (false)
+        }, 13000);
+    }
+
+    useEffect(()=>{
+        change()
+    }, [])
     return (
         <div className="Questions">
+            
             <img src= {Logo} className="Logo" alt="Logo"/>
             <div className='containerText'>
-                <p>Quelle est la couleur du cheval blanc d'Henry IV ?</p>
+                {/* {displayQuestion()} */}
+                {questFromPris()}
                 <img src={Alveole} className="Alveole" id="Alveole" alt="Alveole"/>
             </div>
         </div>
     );
 };
 
-export default Questions;
+
+export default Questions
