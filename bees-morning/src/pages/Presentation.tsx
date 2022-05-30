1b64e88abe011c7bb0504c8e7e310a2cd2d54d15
Gaspar Mis <gaspar.missiaen@hotmail.fr>
2022-05-30 12:19:55 +0200

    :wip: Create carousel, fix style

--- bees-morning/src/pages/Presentation.tsx
+++ bees-morning/src/pages/Presentation.tsx
@@ -1,102 +1,107 @@
 import { PrismicRichText, usePrismicDocumentByUID } from "@prismicio/react";
-import { SliceZone } from "@prismicio/react";
 import * as prismicH from "@prismicio/helpers";
+import { Carousel } from "react-bootstrap";
 import "../styles/_presentation.scss";
-import { useEffect, useState } from "react";
-import Carousel, { CarouselItem } from "../components/Carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
 
 const Presentation = () => {
-  const [page, setPage] = useState(0);
   const [document] = usePrismicDocumentByUID("display-unit", "presentation");
-  const maxPage = document?.data.body.length;
+  const [index, setIndex] = useState(0);
 
-  const checkDisplay = (index: number) => {
-    if (index === page) {
-      return {};
-    } else {
-      return { display: "none" };
-    }
+  const handleSelect = (selectedIndex: number, e: any) => {
+    setIndex(0);
   };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (page !== maxPage - 1) {
-        setPage(page + 1);
-      } else {
-        setPage(0);
-      }
-    }, 5000);
-    return () => clearTimeout(timer);
-  });
-
-  function TextSliceType1({ slice, index }: any) {
+  function TextSliceType1(slice: any) {
     return (
-      <CarouselItem>
-        <section className="slice-type-1" style={checkDisplay(index)}>
-          <div className="left-info">
-            <div className="content">
-              <PrismicRichText field={slice.primary.left_text} />
+      <Carousel.Item>
+        <Carousel.Caption>
+          <section className="slice-type-1">
+            <div className="left-info">
+              <div className="content">
+                <PrismicRichText field={slice.primary.left_text} />
+              </div>
             </div>
-          </div>
-          <div className="line-break" />
-          <div className="right-info">
-            <div className="content">
-              <PrismicRichText field={slice.primary.right_text} />
+            <div className="line-break" />
+            <div className="right-info">
+              <div className="content">
+                <PrismicRichText field={slice.primary.right_text} />
+              </div>
             </div>
-          </div>
-        </section>
-      </CarouselItem>
+          </section>
+        </Carousel.Caption>
+      </Carousel.Item>
     );
   }
 
-  function TextSliceType2({ slice, index }: any) {
+  function TextSliceType2(slice: any) {
     const src = prismicH.asImageSrc(slice.primary.image);
     if (src !== null) {
       return (
-        <CarouselItem>
-          <section className="slice-type-2" style={checkDisplay(index)}>
-            <img
-              className="img-center"
-              src={src}
-              alt={slice.primary.image.alt}
-            />
-          </section>
-        </CarouselItem>
+        <Carousel.Item>
+          <Carousel.Caption>
+            <section className="slice-type-2">
+              <img
+                className="img-center"
+                src={src}
+                alt={slice.primary.image.alt}
+              />
+            </section>
+          </Carousel.Caption>
+        </Carousel.Item>
       );
     } else {
       return (
-        <CarouselItem>
-          <section className="slice-type-2">
-            <img className="img-center" alt={slice.primary.image.alt} />
-          </section>
-        </CarouselItem>
+        <Carousel.Item>
+          <Carousel.Caption>
+            <section className="slice-type-2">
+              <img className="img-center" alt={slice.primary.image.alt} />
+            </section>
+          </Carousel.Caption>
+        </Carousel.Item>
       );
     }
   }
 
-  function TextSliceType3({ slice, index }: any) {
+  function TextSliceType3(slice: any) {
     return (
-      <CarouselItem>
-        <section className="slice-type-3" style={checkDisplay(index)}>
-          <div className="text">
-            <PrismicRichText field={slice.primary.center_text} />
-          </div>
-        </section>
-      </CarouselItem>
+      <Carousel.Item>
+        <Carousel.Caption>
+          <section className="slice-type-3">
+            <div className="text">
+              <PrismicRichText field={slice.primary.center_text} />
+            </div>
+          </section>
+        </Carousel.Caption>
+      </Carousel.Item>
     );
   }
 
   return (
-    <Carousel>
-      <SliceZone
-        slices={document?.data.body}
-        components={{
-          presentation_type_1: TextSliceType1,
-          presentation_type_2: TextSliceType2,
-          presentation_type_3: TextSliceType3,
-        }}
-      />
-    </Carousel>
+    <div className="carousel-wrapper">
+      <Carousel
+        fade
+        controls={false}
+        keyboard={false}
+        touch={false}
+        activeIndex={index}
+        onSelect={handleSelect}
+        indicators={false}
+      >
+        {document?.data.body.map((element: any) => {
+          switch (element.slice_type) {
+            case "presentation_type_1":
+              return TextSliceType1(element);
+            case "presentation_type_2":
+              return TextSliceType2(element);
+            case "presentation_type_3":
+              return TextSliceType3(element);
+          }
+          return "";
+        })}
+      </Carousel>
+    </div>
   );
 };
 
