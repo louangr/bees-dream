fb34c1a39be1339b8cd9b10f777b03eabad3a9cd
Gaspar Mis <gaspar.missiaen@hotmail.fr>
2022-05-31 10:37:07 +0200

    :wrench: fix question carousel

--- bees-morning/src/App.tsx
+++ bees-morning/src/App.tsx
@@ -5,26 +5,41 @@ import Questions from "./components/Questions";
 import { usePrismicDocumentByUID } from "@prismicio/react";
 
 function App() {
-  const [document] = usePrismicDocumentByUID("quizz-question", "questions");
+  const [document, documentState] = usePrismicDocumentByUID(
+    "quizz-question",
+    "questions"
+  );
   const [indexQuestion, setIndexQuestion] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (indexQuestion < document?.data?.body?.length - 1) {
-        setIndexQuestion(indexQuestion + 1);
-      } else if (indexQuestion === document?.data?.body?.length - 1) {
-        setIndexQuestion(0);
-      }
-    }, 2000);
-    console.log(indexQuestion);
-    return () => clearInterval(interval);
-  }, []);
+    if (documentState.state === "failed") {
+      console.warn(
+        "Blog Home document was not found. Make sure it exists in your Prismic repository"
+      );
+    } else {
+      const numberQuestion = document?.data.body.length;
+
+      console.log(document);
+      console.log(numberQuestion);
+      const interval = setInterval(() => {
+        if (indexQuestion < numberQuestion - 1) {
+          setIndexQuestion(indexQuestion + 1);
+        } else if (indexQuestion === numberQuestion - 1) {
+          setIndexQuestion(0);
+        }
+      }, 4000);
+      console.log(indexQuestion);
+      return () => clearInterval(interval);
+    }
+  }, [documentState.state, indexQuestion, document]);
 
   return (
     <div>
       <h1 className="title">Bee's Morning</h1>
       <Presentation />
-      <Questions value={document?.data.body?.[indexQuestion].primary} />
+      {document && (
+        <Questions value={document?.data.body?.[indexQuestion].primary} />
+      )}
     </div>
   );
 }
