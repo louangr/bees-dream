74875a59567881f734ec7e793a9379d19b145018
Louan GERAY <louan.geray@etu.univ-nantes.fr>
2022-05-31 14:43:53 +0000

    Update API/internal/web/handler/monodose/monodose.go

--- API/internal/web/handler/monodose/monodose.go
+++ API/internal/web/handler/monodose/monodose.go
@@ -172,5 +172,7 @@ func (m MonodoseRoutes) Update(w http.ResponseWriter, r *http.Request) {
 
 func (m MonodoseRoutes) CORSVerification(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", "ouiuiui")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,OPTIONS")
+	fmt.Fprintf(w, "%s", "{}")
 }
